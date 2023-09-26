47fe0b5e21f2f6da18bb60237e90f2fe62ee7464
ChrysostomosKas <xrysostomoskasapidis@hotmail.com>
2023-09-26 23:20:27 +0300

    Add enum for pet categories

---
+++ app/Enums/PetCategory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Enums;
+
+enum PetCategory: string
+{
+    case Dog = 'Dog';
+    case Cat = 'Cat';
+    case Bird = 'Bird';
+    case Fish = 'Fish';
+    case Rabbit = 'Rabbit';
+    case Hamster = 'Hamster';
+    case GuineaPig = 'Guinea Pig';
+    case Reptile = 'Reptile';
+    case Amphibian = 'Amphibian';
+    case Horse = 'Horse';
+    case Other = 'Other';
+
+    public function name(): string
+    {
+        return match ($this) {
+            self::Dog => 'Σκύλος',
+            self::Cat => 'Γάτα',
+            self::Bird => 'Πτηνό',
+            self::Fish => 'Ψάρι',
+            self::Rabbit => 'Κουνέλι',
+            self::Hamster => 'Χάμστερ',
+            self::GuineaPig => 'Ινδικό χοιρίδιο',
+            self::Reptile => 'Ερπετό',
+            self::Amphibian => 'Αμφίβιο',
+            self::Horse => 'Άλογο',
+            self::Other => 'Άλλο',
+        };
+    }
+}
