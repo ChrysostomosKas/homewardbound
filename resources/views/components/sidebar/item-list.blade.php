81481d5696da044fabad8be6570171d8b3b3c7ca
ChrysostomosKas <xrysostomoskasapidis@hotmail.com>
2023-09-26 23:53:39 +0300

    Add minor changes

--- resources/views/components/sidebar/item-list.blade.php
+++ resources/views/components/sidebar/item-list.blade.php
@@ -1,4 +1,5 @@
 <x-sidebar.item href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard.*')" icon="home">{{ __('Home') }}</x-sidebar.item>
+<x-sidebar.item href="{{ route('profile.edit') }}" :active="request()->routeIs('profile.*')" icon="user">{{ __('Profile') }}</x-sidebar.item>
 <div class="px-2 py-1">
     <x-button.logout/>
 </div>
