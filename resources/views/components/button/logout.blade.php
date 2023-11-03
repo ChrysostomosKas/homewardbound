2139a5f0f42a5fd7216b2e28f99a739394102e6e
ChrysostomosKas <xrysostomoskasapidis@hotmail.com>
2023-11-04 00:07:44 +0200

    Add icons to the app

--- resources/views/components/button/logout.blade.php
+++ resources/views/components/button/logout.blade.php
@@ -1,7 +1,7 @@
 <form method="POST" action="{{ route('logout') }}">
     @csrf
     <button class="text-indigo-100 hover:bg-gray-700 hover:text-white group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium" type="submit">
-        <x-svg svg="logout" class="h-6 w-6" />
+        <x-tabler-logout class="w-6 h-6 text-white"/>
         {{ __('Log out') }}
     </button>
 </form>
--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -1,14 +1,15 @@
 @props(['active', 'count', 'icon'])
 
 @php
-    $classes = ($active ?? false )
-                ? 'text-indigo-100 hover:bg-gray-900 hover:text-white group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium'
-                : 'text-indigo-100 hover:bg-gray-700 hover:text-white group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium';
+    $isActive = $active ?? false;
+    $classes = ($isActive)
+        ? 'text-indigo-100 hover:bg-gray-900 hover:text-white group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium'
+        : 'text-indigo-100 hover:bg-gray-700 hover:text-white group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
     @if (isset($icon))
-        <x-svg svg="{{ $icon }}" class="h-6 w-6" color="white"/>
+        <x-dynamic-component :component="'tabler-'.$icon" class="w-6 h-6 text-white" />
     @endif
     {{ $slot }}
 </a>
