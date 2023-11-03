ef03a5dfb24a39de1d7ea669bff956eef517de78
ChrysostomosKas <xrysostomoskasapidis@hotmail.com>
2023-11-04 01:17:43 +0200

    Implement slugs for breed seeders to improve URL readability

--- database/migrations/2023_11_03_221415_create_cat_breeds_table.php
+++ database/migrations/2023_11_03_221415_create_cat_breeds_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name_gr');
             $table->string('name_en');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
