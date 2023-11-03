fdb7d7201f94d5b6d18c7889f4dc783a83563974
ChrysostomosKas <xrysostomoskasapidis@hotmail.com>
2023-11-04 01:56:38 +0200

    Add temporary icons to item-list

--- resources/views/components/sidebar/item-list.blade.php
+++ resources/views/components/sidebar/item-list.blade.php
@@ -1,5 +1,11 @@
 <x-sidebar.item href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard.*')" icon="home">{{ __('Home') }}</x-sidebar.item>
 <x-sidebar.item href="{{ route('profile.edit') }}" :active="request()->routeIs('profile.*')" icon="user">{{ __('Profile') }}</x-sidebar.item>
+<x-sidebar.item href="#" :active="request()->routeIs('#.*')" icon="paw">{{ __('Pet List') }}</x-sidebar.item>
+<x-sidebar.item href="#" :active="request()->routeIs('#.*')" icon="heartbeat">{{ __('Adoption') }}</x-sidebar.item>
+<x-sidebar.item href="#" :active="request()->routeIs('#.*')" icon="medical-cross">{{ __('Medical') }}</x-sidebar.item>
+<x-sidebar.item href="#" :active="request()->routeIs('#.*')" icon="map-pin">{{ __('Pet Reports') }}</x-sidebar.item>
+<x-sidebar.item href="#" :active="request()->routeIs('#.*')" icon="presentation-analytics">{{ __('Analytics') }}</x-sidebar.item>
+<x-sidebar.item href="#" :active="request()->routeIs('#.*')" icon="device-analytics">{{ __('Statistics ') }}</x-sidebar.item>
 <div class="px-2 py-1">
     <x-button.logout/>
 </div>
