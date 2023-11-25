e186924e56473987d1d9477f92deec332fa58f02
ChrysostomosKas <xrysostomoskasapidis@hotmail.com>
2023-11-25 23:14:52 +0200

    Add livewire-charts package

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,52 +1,53 @@
-    <!DOCTYPE html>
-    <html class="h-full bg-gray-50" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<!DOCTYPE html>
+<html class="h-full bg-gray-50" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-        @vite('resources/css/app.css')
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    @vite('resources/css/app.css')
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @stack('header_scripts')
-        @stack('scripts')
-        @livewireStyles
-        @filamentStyles
-    </head>
-    <body class="h-full overflow-hidden">
-    <div class="h-full flex">
-        <div class="hidden w-28 bg-gray-800 md:block">
-            <div class="w-full py-6 flex flex-col items-center">
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @stack('header_scripts')
+    @stack('scripts')
+    @livewireStyles
+    @filamentStyles
+</head>
+<body class="h-full overflow-hidden">
+<div class="h-full flex">
+    <div class="hidden w-28 bg-gray-800 md:block">
+        <div class="w-full py-6 flex flex-col items-center">
 
-                <div class="flex-1 mt-6 w-full px-2 space-y-1">
-                    <x-sidebar.item-list/>
-                </div>
+            <div class="flex-1 mt-6 w-full px-2 space-y-1">
+                <x-sidebar.item-list/>
             </div>
         </div>
-
-        <x-sidebar.mobile/>
-
-        <!-- Content area -->
-        <div class="flex-1 flex flex-col overflow-hidden">
-
-            <x-notification/>
-
-            <x-dash-nav/>
-
-            <x-dash-container>
-
-                {{ $slot }}
-
-            </x-dash-container>
-        </div>
     </div>
-    @livewireScripts
-    @filamentScripts
-    </body>
-    </html>
+
+    <x-sidebar.mobile/>
+
+    <!-- Content area -->
+    <div class="flex-1 flex flex-col overflow-hidden">
+
+        <x-notification/>
+
+        <x-dash-nav/>
+
+        <x-dash-container>
+
+            {{ $slot }}
+
+        </x-dash-container>
+    </div>
+</div>
+@livewireScripts
+@filamentScripts
+@livewireChartsScripts
+</body>
+</html>
