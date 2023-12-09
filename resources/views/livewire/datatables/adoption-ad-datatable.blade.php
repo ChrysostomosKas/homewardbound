807864621546dc113a6221bcd3f99bbfa79adba6
ChrysostomosKas <xrysostomoskasapidis@hotmail.com>
2023-12-09 14:07:41 +0200

    Change the layout of adoption-ad datatable

--- resources/views/livewire/datatables/adoption-ad-datatable.blade.php
+++ resources/views/livewire/datatables/adoption-ad-datatable.blade.php
@@ -3,20 +3,34 @@
         <div class="relative mx-auto max-w-7xl">
             <div class="mx-auto mt-12 grid gap-5 lg:max-w-none lg:grid-cols-3 p-4">
                 @foreach($adoptionAds as $ad)
-                <div class="flex flex-col overflow-hidden rounded-lg shadow-lg h-[400px] transform hover:scale-105 transition-all ease-in-out duration-300 hover:cursor-pointer">
-                    <div class="flex flex-1 flex-col justify-between bg-white p-6">
-                        <div class="flex-shrink-0">
-                            <img class="h-48 w-full object-cover rounded" src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">
-                        </div>
-                        <div class="flex flex-col justify-between flex-1 p-6 bg-white">
-                            <div class="flex-1">
-                                <p class="text-sm font-medium text-indigo-600 text-center">{{ $ad->created_at->format('m-d-Y') }}</p>
-                                <p class="text-lg font-semibold text-gray-900 text-center line-clamp-1">{{ $ad->title }}</p>
-                                <p class="mt-3 text-base text-gray-500 line-clamp-4">{{ $ad->description }}</p>
+                    <div
+                        class="flex flex-col overflow-hidden rounded-lg shadow-lg h-[500px] transform hover:scale-105 transition-all ease-in-out duration-300 hover:cursor-pointer">
+                        <img class="rounded-t-lg"
+                             src="https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80"
+                             alt=""/>
+                        <div class="py-6 px-8 rounded-lg bg-white">
+                            <h1 class="text-gray-700 font-bold text-2xl mb-3 hover:text-gray-900 hover:cursor-pointer">
+                                I'm dog
+                                for you.</h1>
+                            <p class="text-gray-700 tracking-wide line-clamp-4">Lorem ipsum dolor sit amet consectetur,
+                                adipisicing elit.
+                                Eum, labore. Ea debitis beatae sequi deleniti.</p>
+                            <div class="flex justify-between">
+                                <button
+                                    class="mt-6 py-2 px-4 bg-yellow-400 text-gray-800 font-bold rounded-lg shadow-md hover:shadow-lg transition duration-300">
+                                    Show
+                                </button>
+                                <div class="flex items-center mt-6 gap-2">
+                                    <x-tabler-heart
+                                        class="w-8 h-8 text-gray-300 hover:fill-neutral-400 hover:text-gray-400"/>
+                                    <span>2.089 likes</span>
+                                </div>
                             </div>
                         </div>
+                        <div class="absolute top-2 right-2 py-2 px-4 bg-white rounded-lg">
+                            <span class="text-sm font-semibold">09-12-2023</span>
+                        </div>
                     </div>
-                </div>
                 @endforeach
             </div>
             <div class="mt-6">
