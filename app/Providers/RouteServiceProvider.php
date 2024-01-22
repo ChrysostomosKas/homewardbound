482f6f2ae54575901ef8e2e5f4c6d0e3a061c9c7
ChrysostomosKas <xrysostomoskasapidis@hotmail.com>
2024-01-22 23:08:02 +0200

    Add minor changes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,16 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/login';
+    public const HOME = '/dashboard';
+
+    /**
+     * The path to your application's "home" route.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @var string
+     */
+    public const Login = '/login';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
