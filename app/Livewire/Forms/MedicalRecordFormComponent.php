bc4ccda59eed6f7149e90c4adc68d0d4b64f91a0
ChrysostomosKas <xrysostomoskasapidis@hotmail.com>
2024-01-29 23:29:24 +0200

    Add changes to MedicalRecordFormComponent

--- app/Livewire/Forms/MedicalRecordFormComponent.php
+++ app/Livewire/Forms/MedicalRecordFormComponent.php
@@ -19,17 +19,27 @@ class MedicalRecordFormComponent extends Component implements HasForms
     use InteractsWithForms;
 
     public MedicalRecord $medicalRecord;
+    public Pet $pet;
     public string $medical_history = '';
     public string $medications = '';
     public string $allergies = '';
     public string $emergency_contact_number = '';
     public string $spaying_neutering_date = '';
     public string $behavioral_notes = '';
+    public string $name = '';
+    public string $species = '';
+    public string $breed = '';
+    public string $age = '';
+    public string $weight = '';
+    public string $color = '';
+    public string $special_needs = '';
+    public string $microchip_number = '';
 
     public function mount($medicalForm_id = null): void
     {
         if ($medicalForm_id) {
             $this->medicalRecord = MedicalRecord::find($medicalForm_id);
+            $this->pet = Pet::find($this->medicalRecord->pet_id);
 
             $this->form->fill([
                 'medical_history' => $this->medicalRecord->medical_history,
@@ -37,7 +47,15 @@ class MedicalRecordFormComponent extends Component implements HasForms
                 'allergies' => $this->medicalRecord->allergies,
                 'emergency_contact_number' => $this->medicalRecord->emergency_contact_number,
                 'spaying_neutering_date' => $this->medicalRecord->spaying_neutering_date,
-                'behavioral_notes' => $this->medicalRecord->behavioral_notes
+                'behavioral_notes' => $this->medicalRecord->behavioral_notes,
+                'name' => $this->pet->name,
+                'species' => $this->pet->species,
+                'breed' => $this->pet->breed,
+                'age' => $this->pet->age,
+                'weight' => $this->pet->weight,
+                'color' => $this->pet->color,
+                'special_needs' => $this->pet->special_needs,
+                'microchip_number' => $this->pet->microchip_number
             ]);
         } else {
             $this->form->fill([
@@ -47,15 +65,80 @@ class MedicalRecordFormComponent extends Component implements HasForms
                 'emergency_contact_number' => '',
                 'spaying_neutering_date' => '',
                 'behavioral_notes' => '',
+                'name' => '',
+                'species' => '',
+                'breed' => '',
+                'age' => '',
+                'weight' => '',
+                'color' => '',
+                'special_needs' => '',
+                'microchip_number' => ''
             ]);
 
             $this->medicalRecord = $this->makeBlankMedicalRecordForm();
+            $this->pet = $this->makeBlankPetForm();
         }
     }
 
     protected function getFormSchema(): array
     {
         return [
+            Section::make('Pet Form')
+                ->description('Provide some information about your pet.')
+                ->schema([
+                    Grid::make(2)
+                        ->schema([
+                            TextInput::make('name')
+                                ->label('Name')
+                                ->columnSpan(1)
+                                ->required(),
+                            TextInput::make('species')
+                                ->label('Species')
+                                ->columnSpan(1)
+                                ->required(),
+                        ]),
+                    Grid::make(2)
+                        ->schema([
+                            TextInput::make('breed')
+                                ->label('Breed')
+                                ->columnSpan(1)
+                                ->required(),
+                            TextInput::make('age')
+                                ->label('Age')
+                                ->integer()
+                                ->columnSpan(1)
+                                ->required(),
+                        ]),
+                    Grid::make(2)
+                        ->schema([
+                            TextInput::make('weight')
+                                ->label('Weight')
+                                ->integer()
+                                ->minValue(0)
+                                ->columnSpan(1)
+                                ->nullable(),
+                            TextInput::make('color')
+                                ->label('Color')
+                                ->columnSpan(1)
+                                ->nullable(),
+                        ]),
+                    Grid::make(1)
+                        ->schema([
+                            TextInput::make('microchip_number')
+                                ->label('Microchip Number')
+                                ->columnSpan(1)
+                                ->nullable()
+                        ]),
+                    Grid::make(1)
+                        ->schema([
+                            Textarea::make('special_needs')
+                                ->label('Special Needs')
+                                ->columnSpan(1)
+                                ->rows(5)
+                                ->cols(5)
+                                ->nullable()
+                        ])
+                ]),
             Section::make('Pet Medical Record Form')
                 ->description('Provide the following information about the pets medical history.')
                 ->schema([
@@ -115,16 +198,35 @@ class MedicalRecordFormComponent extends Component implements HasForms
         return MedicalRecord::make(['created_at' => now()]);
     }
 
+    public function makeBlankPetForm()
+    {
+        return Pet::make(['created_at' => now()]);
+    }
+
     /**
      * @throws ValidationException
      */
     public function saveMedicalRecord()
     {
+        $formFields = $this->form->getState();
+        $medicalRecordFields = ['medical_history', 'medications', 'allergies', 'emergency_contact_number', 'spaying_neutering_date', 'behavioral_notes'];
+        $petFields = ['name', 'species', 'breed', 'age', 'weight', 'color', 'special_needs', 'microchip_number'];
+
+        $medicalRecordData = array_intersect_key($formFields, array_flip($medicalRecordFields));
+        $petData = array_intersect_key($formFields, array_flip($petFields));
+
+        $petData['user_id'] = auth()->id();
+
+        $new_pet = Pet::updateOrCreate(
+            ['id' => $this->pet->id],
+            $petData
+        );
+
+        $medicalRecordData['pet_id'] = $new_pet->id;
+
         $new_medicalRecord = MedicalRecord::updateOrCreate(
             ['id' => $this->medicalRecord->id],
-            $this->form->getState() + [
-                'pet_id' => Pet::inRandomOrder()->first()->id
-            ]
+            $medicalRecordData
         );
 
         if ($new_medicalRecord->id) {
