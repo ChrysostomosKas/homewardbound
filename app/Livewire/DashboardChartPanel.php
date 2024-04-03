7653377fd34c15ee01890abba4543b8653b0016b
ChrysostomosKas <xrysostomoskasapidis@hotmail.com>
2024-04-03 22:53:32 +0300

    Add more statistics on dashboard chart panel

--- app/Livewire/DashboardChartPanel.php
+++ app/Livewire/DashboardChartPanel.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Enums\AdoptionAdStatus;
+use App\Models\AdoptionInterest;
 use App\Models\User;
 use Asantibanez\LivewireCharts\Models\LineChartModel;
 use Illuminate\Support\Facades\Gate;
@@ -25,11 +27,13 @@ class DashboardChartPanel extends Component
         ];
 
         $users = User::all();
+        $requests = AdoptionInterest::where('adoption_interests.status', '=', AdoptionAdStatus::Open->name);
 
         for ($i = 6; $i >= 0; $i--) {
             $subDays = now()->subDays($i);
             $formatted_date = $subDays->format('Y-m-d');
             $lineChartModel->addSeriesPoint(__('Users'), $subDays->format('d/m/Y'), $users->whereBetween('created_at', [$formatted_date . " 00:00:00", $formatted_date . " 23:59:59"])->count());
+            $lineChartModel->addSeriesPoint(__('Requests'), $subDays->format('d/m/Y'), $requests->whereBetween('created_at', [$formatted_date . " 00:00:00", $formatted_date . " 23:59:59"])->count());
         }
 
         return view('livewire.dashboard-chart-panel', $view);
--- app/Models/User.php
+++ app/Models/User.php
@@ -31,7 +31,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'password',
         'provider',
         'provider_id',
-        'provider_token'
+        'provider_token',
+        'username'
     ];
 
     /**
